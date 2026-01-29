e5d39c5e359910ffaf90002c5c0eb79c5e5f946e
sarobidy <rasendraminosarobidy@gmail.com>
2026-01-29 14:08:28 +0300

    Fichier pour tester l'impréssion sans passer par odoo

---
+++ test_print.py
@@ -0,0 +1,16 @@
+from print_server.odoo_client import OdooClient
+from print_server.receipt_formatter import ReceiptFormatter
+from print_server.printer import Printer
+
+client = OdooClient()
+client.connect()
+
+order = input('Numéro de commande à imprimer: ')
+data = client.get_order_data('Restaurant/' + order)
+if data:
+    formatter = ReceiptFormatter()
+    ticket = formatter.format_receipt(data)
+    
+    printer = Printer()
+    result = printer.print_raw(ticket)
+    print('Impression:', 'OK' if result else 'ERREUR')
